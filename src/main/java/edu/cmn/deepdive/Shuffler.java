8eebfa2d7eb045995eaca7d49242fc2502309357
seanblann <sblann@cnm.edu>
2022-01-31 16:14:18 -0700

    Added draw methods with shuffle helpers.

--- src/main/java/edu/cmn/deepdive/Shuffler.java
+++ src/main/java/edu/cmn/deepdive/Shuffler.java
@@ -1,5 +1,6 @@
 package edu.cmn.deepdive;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -21,6 +22,104 @@ public class Shuffler {
     this.rng = rng;
   }
 
+  public boolean[] draw(boolean[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public byte[] draw(byte[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public char[] draw(char[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public double[] draw(double[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public float[] draw(float[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public int[] draw(int[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public long[] draw(long[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public Object[] draw(Object[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public short[] draw(short[] data, int drawSize) {
+    shuffle(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+
+
+
+  /**
+   * Shuffles the contents of {@code data} in place. That is, a new array is not created and
+   * returned; instead, data is modified directly.
+   *
+   * @param data Array being shuffled.
+   */
+  public void shuffle(boolean[] data) {
+    shuffle(data, data.length -1);
+  }
+
+  /**
+   * Shuffles the contents of {@code data} in place. That is, a new array is not created and
+   * returned; instead, data is modified directly.
+   *
+   * @param data Array being shuffled.
+   */
+  public void shuffle(byte[] data) {
+    shuffle(data, data.length -1);
+  }
+
+  /**
+   * Shuffles the contents of {@code data} in place. That is, a new array is not created and
+   * returned; instead, data is modified directly.
+   *
+   * @param data Array being shuffled.
+   */
+  public void shuffle(char[] data) {
+    shuffle(data, data.length -1);
+  }
+
+  /**
+   * Shuffles the contents of {@code data} in place. That is, a new array is not created and
+   * returned; instead, data is modified directly.
+   *
+   * @param data Array being shuffled.
+   */
+  public void shuffle(double[] data) {
+    shuffle(data, data.length -1);
+  }
+
+  /**
+   * Shuffles the contents of {@code data} in place. That is, a new array is not created and
+   * returned; instead, data is modified directly.
+   *
+   * @param data Array being shuffled.
+   */
+  public void shuffle(float[] data) {
+    shuffle(data, data.length -1);
+  }
+
   /**
    * Shuffles the contents of {@code data} in place. That is, a new array is not created and
    * returned; instead, data is modified directly.
@@ -28,9 +127,100 @@ public class Shuffler {
    * @param data Array being shuffled.
    */
   public void shuffle(int[] data) {
-    //Iterate over data from end to start, swapping with a randomly selected element.
-    for (int targetIndex = data.length - 1; targetIndex > 0; targetIndex--) {
-      int sourceIndex = rng.nextInt(targetIndex + 1);
+    shuffle(data, data.length -1);
+  }
+
+  /**
+   * Shuffles the contents of {@code data} in place. That is, a new array is not created and
+   * returned; instead, data is modified directly.
+   *
+   * @param data Array being shuffled.
+   */
+  public void shuffle(long[] data) {
+    shuffle(data, data.length -1);
+  }
+
+  /**
+   * Shuffles the contents of {@code data} in place. That is, a new array is not created and
+   * returned; instead, data is modified directly.
+   *
+   * @param data Array being shuffled.
+   */
+  public void shuffle(Object[] data) {
+    shuffle(data, data.length -1);
+  }
+
+  /**
+   * Shuffles the contents of {@code data} in place. That is, a new array is not created and
+   * returned; instead, data is modified directly.
+   *
+   * @param data Array being shuffled.
+   */
+  public void shuffle(short[] data) {
+    shuffle(data, data.length -1);
+  }
+
+
+
+
+  private void shuffle(boolean[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
+      if (sourceIndex != targetIndex) {
+        boolean temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+  private void shuffle(byte[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
+      if (sourceIndex != targetIndex) {
+        byte temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+  private void shuffle(char[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
+      if (sourceIndex != targetIndex) {
+        char temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+  private void shuffle(double[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
+      if (sourceIndex != targetIndex) {
+        double temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+  private void shuffle(float[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
+      if (sourceIndex != targetIndex) {
+        float temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+  private void shuffle(int[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
       if (sourceIndex != targetIndex) {
         int temp = data[targetIndex];
         data[targetIndex] = data[sourceIndex];
@@ -38,5 +228,40 @@ public class Shuffler {
       }
     }
   }
+
+  private void shuffle(long[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
+      if (sourceIndex != targetIndex) {
+        long temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+  private void shuffle(Object[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
+      if (sourceIndex != targetIndex) {
+        Object temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+  private void shuffle(short[] data, int drawSize) {
+    for (int targetIndex = data.length -1; targetIndex >= data.length - drawSize; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex +1);
+      if (sourceIndex != targetIndex) {
+        short temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+
 
 }
