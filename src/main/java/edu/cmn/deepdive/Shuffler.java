2043898f6d95f5d0b9607d8232aca867446b226d
seanblann <sblann@cnm.edu>
2022-01-31 12:04:39 -0700

    Timed different sources of RNG

--- src/main/java/edu/cmn/deepdive/Shuffler.java
+++ src/main/java/edu/cmn/deepdive/Shuffler.java
@@ -1,5 +1,7 @@
 package edu.cmn.deepdive;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Shuffler {
@@ -12,10 +14,41 @@ public class Shuffler {
   }
 
   public void shuffle(int[] data) {
-    for (int targetIndex = data.length -1; targetIndex > 0; targetIndex--) {
-      int sourceIndex = rng.nextInt(targetIndex +1);
-      int temp = data[targetIndex];
-      data[targetIndex] = data[sourceIndex];
+    for (int targetIndex = data.length - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      if (sourceIndex != targetIndex) {
+        int temp = data[targetIndex];
+        data[targetIndex] = data[sourceIndex];
+        data[sourceIndex] = temp;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    long timerStart;
+    Random rng = new SecureRandom();
+    timerStart = System.currentTimeMillis();
+    repeatShuffles(rng, 100_000, 100);
+    System.out.println(System.currentTimeMillis() - timerStart);
+
+    rng = new Random();
+    timerStart = System.currentTimeMillis();
+    repeatShuffles(rng, 100_000, 100);
+    System.out.println(System.currentTimeMillis() - timerStart);
+
+  }
+
+  private static void repeatShuffles(Random rng, int size, int repetitions) {
+    Shuffler shuffler = new Shuffler(rng);
+    int[] data = new int[size];
+//    System.out.printf("Initial Values = %s%n", Arrays.toString(data));
+    for (int i = 0; i < data.length; i++) {
+      data[i] = i;
+    }
+//    System.out.printf("Unshuffled Values = %s%n",Arrays.toString(data));
+    for (int i = 0; i < repetitions; i++) {
+      shuffler.shuffle(data);
+//    System.out.printf("Shuffled values = %s%n",Arrays.toString(data));
     }
   }
 }
