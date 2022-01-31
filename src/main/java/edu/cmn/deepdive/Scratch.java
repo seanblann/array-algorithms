a56324f406b46d2b10d4985262821dd23bf85a84
seanblann <sblann@cnm.edu>
2022-01-31 16:39:56 -0700

    Exploring List and ArrayList.

--- src/main/java/edu/cmn/deepdive/Scratch.java
+++ src/main/java/edu/cmn/deepdive/Scratch.java
@@ -1,6 +1,9 @@
 package edu.cmn.deepdive;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Scratch {
@@ -9,12 +12,12 @@ public class Scratch {
     long timerStart;
     Random rng = new SecureRandom();
     timerStart = System.currentTimeMillis();
-    repeatShuffles(rng, 100_000, 100);
+    repeatShuffles(rng, 10_000, 100);
     System.out.println(System.currentTimeMillis() - timerStart);
 
     rng = new Random();
     timerStart = System.currentTimeMillis();
-    repeatShuffles(rng, 100_000, 100);
+    repeatShuffles(rng, 10_000, 100);
     System.out.println(System.currentTimeMillis() - timerStart);
 
   }
@@ -22,14 +25,19 @@ public class Scratch {
   private static void repeatShuffles(Random rng, int size, int repetitions) {
     Shuffler shuffler = new Shuffler(rng);
     int[] data = new int[size];
-//    System.out.printf("Initial Values = %s%n", Arrays.toString(data));
+
     for (int i = 0; i < data.length; i++) {
       data[i] = i;
     }
-//    System.out.printf("Unshuffled Values = %s%n",Arrays.toString(data));
+
+    List<Integer> dataList = new ArrayList<>();
+    for (int value : data) {
+      dataList.add(value);
+    }
+
     for (int i = 0; i < repetitions; i++) {
-      shuffler.shuffle(data);
-//    System.out.printf("Shuffled values = %s%n",Arrays.toString(data));
+      Collections.shuffle(dataList, rng);
+//      shuffler.shuffle(data);
     }
   }
 
