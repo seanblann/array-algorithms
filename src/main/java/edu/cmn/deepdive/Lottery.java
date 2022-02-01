72c0c91ceaaa9753d2bcc20e80c5a1c0304d9157
seanblann <sblann@cnm.edu>
2022-02-01 11:00:01 -0700

    Decomposed initialize, pick, & display operations.

--- src/main/java/edu/cmn/deepdive/Lottery.java
+++ src/main/java/edu/cmn/deepdive/Lottery.java
@@ -6,18 +6,42 @@ import java.util.Random;
 
 public class Lottery {
 
-  public static void main(String[] args) {
-    int poolSize = Integer.parseInt(args[0]);
-    int drawSize = Integer.parseInt(args[1]);
-    Random rng = new SecureRandom();
-    Shuffler s = new Shuffler(rng);
-    int[] pool = new int[poolSize];
+  private final int poolSize;
+  private final int drawSize;
+  private final Random rng;
+  private final Shuffler s;
+  private int[] pool;
+
+  public Lottery(String[] args) {
+    poolSize = Integer.parseInt(args[0]);
+    drawSize = Integer.parseInt(args[1]);
+    rng = new SecureRandom();
+    s = new Shuffler(rng);
+    pool = new int[poolSize];
     for (int i = 0; i < poolSize; i++) {
       pool[i] = i +1;
     }
+
+  }
+
+  public int[] draw(boolean sorted) {
     int[] sample = s.draw(pool, drawSize);
-    Arrays.sort(sample);
-    System.out.println(Arrays.toString(sample));
+    if (sorted) {
+      Arrays.sort(sample);
+    }
+    return sample;
+  }
+
+  public void display(int[] draw) {
+    System.out.printf("Drawing %d number from a pool of %d: %s%n",
+        drawSize, poolSize, Arrays.toString(draw));
+  }
+
+
+  public static void main(String[] args) {
+    Lottery lotto = new Lottery(args);
+    int [] draw = lotto.draw(true);
+    lotto.display(draw);
   }
 
 }
